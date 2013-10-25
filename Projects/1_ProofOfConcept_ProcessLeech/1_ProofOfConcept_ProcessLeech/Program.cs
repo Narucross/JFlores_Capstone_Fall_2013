b4b99b43ae5fbe8694313354f892bd4d5a255620
Joshua Flores <jflores@student.neumont.edu>
2013-10-25 14:18:25 -0600

    1_Proof_Of_Concept_Getting_Type_V3
    This version can collect all available processes who have an active
    window and stores them into an array, But this is just to save progress
    before I do some window function stuff...

--- Projects/1_ProofOfConcept_ProcessLeech/1_ProofOfConcept_ProcessLeech/Program.cs
+++ Projects/1_ProofOfConcept_ProcessLeech/1_ProofOfConcept_ProcessLeech/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Diagnostics.Process;
+using System.Diagnostics;
 
 
 namespace _1_ProofOfConcept_ProcessLeech
@@ -11,15 +11,40 @@ namespace _1_ProofOfConcept_ProcessLeech
     {
         static void Main(string[] args)
         {
-			Process[] processes = Process.GetProcessesByName("notepad.exe");
-			int i=0;
-			for(Process currentProcess in processes){
-				i++;
-				String message = "" + i
-					+ ".) For process: " + currentProcess.ProcessName +
-						" : type is :" + currentProcess.GetType().ToString();
-				Console.WriteLine(message);
-			}
+            Console.WriteLine("Starting program");
+            var notepadDoc1 = Process.GetProcessById(9288);
+            var notepadDoc2 = Process.GetProcessById(9312);
+            var notepadDoc3 = Process.GetProcessById(41056);
+            Process[] processes = Process.GetProcesses();
+            Console.WriteLine(""+processes.Length);
+            var proc2  = (from cur in processes where !cur.MainWindowTitle.Equals("") select cur ).ToArray();
+            processes = new Process[] { notepadDoc1, notepadDoc2 ,notepadDoc3};
+            int i = 0;
+            foreach (Process currentProcess in processes)
+            {
+                i++;
+                String message;
+                var processName = currentProcess.ProcessName;
+                var processWindowType = currentProcess.MainWindowHandle.GetType().ToString();
+                var processWindowTypeToString = currentProcess.MainWindowHandle.ToString();
+
+                var containerType = currentProcess.GetType();
+
+                bool something = currentProcess.Container != null;
+                if (something)
+                {
+                    Console.WriteLine("" + i + ".) Has Container");
+                }
+                else {
+                    Console.Write("No Container\n");
+                }
+                
+                object[] obects = new object[] { i, processName, processWindowType, processWindowTypeToString,containerType };
+                message = String.Format("{0}.) For the process: {1}\n\tThe type is : {2}\n\tEntity toString : {3}\n\tProcessType : {4}\n", obects);
+                Console.WriteLine(message);
+                
+            }
+            Console.WriteLine("End of the program Press enter to exit.");
             Console.ReadLine();
         }
     }

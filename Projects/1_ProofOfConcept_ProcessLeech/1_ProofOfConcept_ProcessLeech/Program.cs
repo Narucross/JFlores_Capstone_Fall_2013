b472835ef9ac6ce16defebe0ad736c0eeb9de104
Joshua Flores <jflores@student.neumont.edu>
2013-10-25 14:44:29 -0600

    1_Proof_Of_Concept_Getting_Type_V4
    Success in moving a notepad,

--- Projects/1_ProofOfConcept_ProcessLeech/1_ProofOfConcept_ProcessLeech/Program.cs
+++ Projects/1_ProofOfConcept_ProcessLeech/1_ProofOfConcept_ProcessLeech/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 
 namespace _1_ProofOfConcept_ProcessLeech
@@ -38,7 +39,11 @@ namespace _1_ProofOfConcept_ProcessLeech
                 else {
                     Console.Write("No Container\n");
                 }
-                
+                //TEST REGION
+                if (currentProcess.Id == 9288) {
+                    moveNotepad(currentProcess);
+                }
+                //TEST REGION!
                 object[] obects = new object[] { i, processName, processWindowType, processWindowTypeToString,containerType };
                 message = String.Format("{0}.) For the process: {1}\n\tThe type is : {2}\n\tEntity toString : {3}\n\tProcessType : {4}\n", obects);
                 Console.WriteLine(message);
@@ -46,6 +51,36 @@ namespace _1_ProofOfConcept_ProcessLeech
             }
             Console.WriteLine("End of the program Press enter to exit.");
             Console.ReadLine();
+        }//end of main
+
+        static void moveNotepad(Process notepadProcess) {
+            int x = 740;
+            int y = 360;
+            int Width = 563;
+            int Height = 371;
+            //Begin test
+            Console.WriteLine("=========\tBegin Test?\t=========");
+            String s = Console.ReadLine();
+            var ptrintReference = notepadProcess.MainWindowHandle;
+            if (s.Trim().Equals("quit",StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine("Wimp");
+            }
+            else
+            {
+                MoveWindow(ptrintReference, x, y, Width, Height, true);
+                Console.WriteLine("Was it successful?");
+                s = Console.ReadLine();
+                if (s.Trim().Equals("yes", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine("Hurray!\nHazzah!\nI bid to say I do!");
+                }
+            }
         }
-    }
-}
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, ExactSpelling = true, SetLastError = true)]
+        internal static extern void MoveWindow(IntPtr hwnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+    }//end of class
+}//end of namespace

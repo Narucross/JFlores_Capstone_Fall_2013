394b41652cea2b7b51f36ee4aeb0170e2865d106
Joshua Flores <jflores@student.neumont.edu>
2013-10-29 02:29:49 -0600

    Initial_Serialization_Commit
    Created the project to start different saving methods for templates
    updated all Summary for last week, and backlog as well.

---
+++ Projects/PracticeProjects/Binary_Serialization/Store_Rectangles/Store_Rectangles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_Rectangles
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Fill after we know how we want to store the data.
+            /*
+             * Here is the syntax we will probably settle on with in json format:
+              {
+                "Template-Name":"{0}",
+                "Windows" : [
+                    {
+                        "Window process Name" : "{1}",
+                        "Window number" : "{2}",
+                        "Width" : "{3}",
+                        "Height" : "{4}",
+                        "Pos-X" : "{5}",
+                        "Pos-Y" : "{6}",
+                    }
+                ]
+              }           
+             **/
+        }
+    }//end of class
+}//end of namespace

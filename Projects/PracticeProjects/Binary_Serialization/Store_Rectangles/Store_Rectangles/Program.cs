10da076e853046e1945dd3a37df831816dc48d81
Joshua Flores <jflores@student.neumont.edu>
2013-10-29 15:01:16 -0600

    2_Serializations_Experiments_V1
    Implemented a JSon serializer for the "framework" of the template and
    saved applications feature of the project, Didn't include vendor because
    it was too big, need to fix that.

--- Projects/PracticeProjects/Binary_Serialization/Store_Rectangles/Store_Rectangles/Program.cs
+++ Projects/PracticeProjects/Binary_Serialization/Store_Rectangles/Store_Rectangles/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Store_Rectangles
 {
@@ -11,6 +12,19 @@ namespace Store_Rectangles
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Beginning application");
+            SavedAppications applicationTest = new SavedAppications();
+            int gWid=150;
+            int gHei = 100;
+            SavedWindow window1 = newSwindow("Notepad.exe", 1, gWid, gHei, 0, 0);
+            SavedWindow window2 = newSwindow("notepad2.exe", 2, gWid, gHei, 12, 50);
+            SavedWindow window3 = newSwindow("firefox.exe", 4, gWid, gHei, 100, 500);
+            applicationTest.Add(window1);
+            applicationTest.Add(window2);
+            applicationTest.Add(window3);
+            string s = JsonConvert.SerializeObject(applicationTest);
+            Console.WriteLine(String.Format("The serialized Object is:\n{0}", s));
+            
             // Fill after we know how we want to store the data.
             /*
              * Here is the syntax we will probably settle on with in json format:
@@ -28,6 +42,14 @@ namespace Store_Rectangles
                 ]
               }           
              **/
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+            Console.WriteLine("Ending application");
+        }
+
+        static SavedWindow newSwindow(String procName, short tempNum, int width, int height, int pos_x, int pos_y){
+            return new SavedWindow(){WindowProcessName=procName,WindowTempateNumber=tempNum,Width=width,Height=height,Pos_X=pos_x,Pos_Y=pos_y};
         }
     }//end of class
 }//end of namespace
---
+++ Projects/PracticeProjects/Binary_Serialization/Store_Rectangles/Store_Rectangles/SavedApplications.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_Rectangles
+{
+    /// <summary>
+    /// A "Bean" for saving what the user wants the idea is to have a template and application coupling, this being the later.
+    /// </summary>
+    class SavedAppications
+    {
+        public SavedAppications()
+        {
+            SavedWindows = new List<SavedWindow>();
+            // Need to know if overriding constructor will change the properties in any way...
+        }
+
+
+        /// <summary>
+        /// Another word for "TemplateReference"
+        /// </summary>
+        public String TempateName { get; set; }
+        public List<SavedWindow> SavedWindows { get; set; }
+
+        #region convience Methods
+
+        public void Add(SavedWindow newWindow)
+        {
+            this.SavedWindows.Add(newWindow);
+        }
+
+        public bool Remove(SavedWindow window)
+        {
+            return this.SavedWindows.Remove(window);
+        }
+        #endregion
+    }
+}

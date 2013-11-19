dc49296aa890545ac9b9c3eb14ee94c84c0f3fb1
Joshua Flores <jflores@student.neumont.edu>
2013-11-19 15:23:51 -0700

    4_Intermediate_Interface_V3
    Drag and drop application demo is completed:
    revelation as a result:
    Current Intermediate Interface V2-3 are not compatible with final
    project scope implementation of drag and drop. (check worklog for
    details on Week 7)

---
+++ Projects/PracticeProjects/WPF_Application_Drop/ApplicationDrop/ApplicationDrop/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApplicationDrop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            String[] strings = new String[]{"John", "Jacob", "jinklehiemer"};
+            Applications_Domain.ItemsSource = strings;
+            
+        }
+
+        private void Applications_Domain_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void Applications_Domain_MouseMove(object sender, MouseEventArgs e)
+        {
+            string name = Applications_Domain.SelectedItem as String;
+
+            if (name != null && e.LeftButton == MouseButtonState.Pressed)
+            {
+                DataObject StringToCopy = new DataObject(DataFormats.Text, name);
+                DragDrop.DoDragDrop(Applications_Domain, StringToCopy, DragDropEffects.Copy);
+            }
+        }
+
+        private void Applications_Domain_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Drawing_Area_DragEnter(object sender, DragEventArgs e)
+        {
+            Drawing_Area.Opacity = .75;
+
+        }
+
+        private void Drawing_Area_DragLeave(object sender, DragEventArgs e)
+        {
+            Drawing_Area.Opacity = 1;
+            
+        }
+
+        private void Drawing_Area_Drop(object sender, DragEventArgs e)
+        {
+            Label label = new Label();
+            label.Content = e.Data.GetData(DataFormats.Text);
+            Point labelCord = e.GetPosition(Drawing_Area);
+            Canvas.SetLeft(label, labelCord.X);
+            Canvas.SetTop(label, labelCord.Y);
+            Drawing_Area.Children.Add(label);
+        }
+
+        private void EditTextLabel_Drop(object sender, DragEventArgs e)
+        {
+            EditTextLabel.Content = e.Data.GetData(DataFormats.Text);
+        }
+    }
+}

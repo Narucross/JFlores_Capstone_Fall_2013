25fff95ef8402a49f8f73d2b4aaa5ce24ac5a4bc
Joshua Flores <jflores@student.neumont.edu>
2013-11-25 16:23:22 -0700

    4_Intermediate_Interface_V4
    Draw and drag is working, with the new userControl,
    We are now in a state where we accept drag and drop objects into them

---
+++ Projects/PracticeProjects/Wpf_Intermediate_Interface/DragAndDrop_2/DragAndDrop_2/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using BorderTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DragAndDrop_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Pan_And_Selection_View_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Source == RectangleCreator)
+            {
+                DrawingArea.Background = Brushes.Blue;
+                currentState = drawingState.CreateNewBounds;
+            }
+            else if (e.Source == PanView)
+            {
+                DrawingArea.Background = Brushes.Cyan;
+                currentState = drawingState.PanView;
+            }
+        }
+
+        private void DrawingArea_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            startingPoint = e.GetPosition(DrawingArea);
+                if (e.RightButton == MouseButtonState.Pressed && currentState != drawingState.PanView)
+            {
+                currentState = drawingState.RightClickOnCreateBounds;
+            }
+            else 
+                if (currentState == drawingState.CreateNewBounds)
+            {
+                // Creating and tracking a new Zone
+                currentZone = new ResizeZone();
+                currentZone.Opacity = .65;
+                currentZone.MinWidth = 25;
+                currentZone.MinHeight = 25;
+                Canvas.SetLeft(currentZone, startingPoint.X);
+                Canvas.SetTop(currentZone, startingPoint.Y);
+                DrawingArea.Children.Add(currentZone);
+                currentZone.number = DrawingArea.Children.Count;
+            }
+        }
+        
+        private void DrawingArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point mousePositionRDrawingArea = e.GetPosition(DrawingArea);
+            if (IsCurrentState(drawingState.CreateNewBounds) && currentZone != null)// On creation
+            {
+                Point mpRectangle = mousePositionRDrawingArea;
+                double widthDiff = Math.Abs((startingPoint.X - mpRectangle.X));
+                double heightDiff = Math.Abs((startingPoint.Y - mpRectangle.Y));
+
+                if (mpRectangle.X < startingPoint.X)
+                {
+                    Canvas.SetLeft(currentZone, mpRectangle.X);
+                }
+                if (mpRectangle.Y < startingPoint.Y)
+                {
+                    Canvas.SetTop(currentZone, mpRectangle.Y);
+                }
+                currentZone.Width = widthDiff;
+                currentZone.Height = heightDiff;
+            }
+        }
+
+        private void DrawingArea_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (IsCurrentState(drawingState.CreateNewBounds))
+            {
+                if (currentZone != null)
+                { currentZone.IsEnabled = true; }
+            }
+
+            if (IsCurrentState(drawingState.RightClickOnCreateBounds))
+            {
+                currentState = drawingState.CreateNewBounds;
+            }
+
+            currentZone = null;
+            startingPoint = new Point();
+        }
+
+
+        #region Properties
+
+        private enum drawingState : byte { PanView, CreateNewBounds, RightClickOnCreateBounds }
+
+        drawingState currentState = drawingState.CreateNewBounds;
+
+
+        private bool IsCurrentState(drawingState variable)
+        {
+            return currentState == variable;
+        }
+
+        ResizeZone currentZone;
+        Point startingPoint;
+
+        #endregion
+    }// end of class
+}
---
+++ Projects/PracticeProjects/Wpf_Intermediate_Interface/DragAndDrop_2/DragAndDrop_2/ZoneResize/ResizeZone.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BorderTesting
+{
+    /// <summary>
+    /// Interaction logic for ResizeZone.xaml
+    /// </summary>
+    public partial class ResizeZone : UserControl
+    {
+
+
+        public ResizeZone()
+        {
+            number = 0;
+            InitializeComponent();
+            DataContext = this;
+            IsEnabled = false;
+        }
+        public int number { get; set; }
+
+        public ResizeZone(UIElement parentElement)
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MainBorder.BorderBrush = Brushes.Green;
+        }
+
+        private void UserControl_MouseLeave(object sender, MouseEventArgs e)
+        {
+            MainBorder.BorderBrush = Brushes.Black;
+            Canvas.SetZIndex(this, 0);
+        }
+
+        Point offSet;
+        bool isPressed;
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            isPressed = e.ButtonState == MouseButtonState.Pressed;
+            if (isPressed)
+            {
+                var parent = this.Parent as Canvas;
+                offSet = e.GetPosition(this);
+                if (parent != null)
+                {
+                    Canvas.SetZIndex(this, 1);
+                    Point mousePoint = e.GetPosition(parent);
+                    double offSetX = Canvas.GetLeft(this);
+                    double offSetY = Canvas.GetTop(this);
+                    offSetX -= mousePoint.X;
+                    offSetY -= mousePoint.Y;
+                    offSet = new Point(offSetX, offSetY);
+                }
+            }
+        }
+
+        private void UserControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPressed)
+            {
+                Point currentMousePosition = e.GetPosition(Window.GetWindow(this));
+                double distX = offSet.X + currentMousePosition.X;
+                double distY = offSet.Y + currentMousePosition.Y;
+                Canvas.SetLeft(this, distX);
+                Canvas.SetTop(this, distY);
+            }
+        }
+
+        private void UserControl_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isPressed = false;
+        }
+
+    }//end of class
+}//end of namespace

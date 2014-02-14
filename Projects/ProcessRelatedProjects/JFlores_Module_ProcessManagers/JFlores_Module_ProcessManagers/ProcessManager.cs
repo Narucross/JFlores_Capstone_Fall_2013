5cc77ef3f9cafd2ff3b618bb0cddacd2915e87ba
Joshua Flores <jflores@student.neumont.edu>
2014-02-14 08:33:00 -0700

    A1_Overhall_6 Processes
    Created a ProcessManager library in the project, so that all future
    iterations of every component can just reference it, and maintain the
    updates

---
+++ Projects/ProcessRelatedProjects/JFlores_Module_ProcessManagers/JFlores_Module_ProcessManagers/ProcessManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace JFlores_Module_ProcessManagers
+{
+    public class ProcessManager
+    {
+
+        public static IEnumerable<Process> AllProcessesWithHandles()
+        {
+            return from cal in Process.GetProcesses() where (IntPtr.Zero != (cal.MainWindowHandle)) orderby cal.ProcessName select cal;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, ExactSpelling = true, SetLastError = true)]
+        internal static extern void MoveWindow(IntPtr hwnd, int x, int y, int nWidth, int nHeight, bool bRepaint);
+
+
+    }//end of class
+}//End of namespace

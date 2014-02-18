b1f428aa63f581d794954fceb88df9d9d906c0d9
Joshua Flores <jflores@student.neumont.edu>
2014-02-18 10:49:03 -0700

    8T_Applications_Lists_1_ListBox_ReArrange_1
    Created a new project and begging framework before school commute and
    forced stop.
    This project is intended to test out theories and implementation for the
    re-rearrange behavior in listboxes and by extention the applist
    eventually.

---
+++ Projects/WPF_Basic_Interface/Listbox_Drag_and_Drop_Tests/Listbox_Drag_and_Drop_Tests/Models/CardItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listbox_Drag_and_Drop_Tests.Models
+{
+    public class CardItem
+    {
+        public string ProductName { get; set; }
+
+        public int ProductPrice { get; set; }
+    }
+}
---
+++ Projects/WPF_Basic_Interface/Listbox_Drag_and_Drop_Tests/Listbox_Drag_and_Drop_Tests/StoreFunctionalityViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Listbox_Drag_and_Drop_Tests.Models;
+
+namespace Listbox_Drag_and_Drop_Tests
+{
+    public class StoreFunctionalityViewModel
+    {
+        public List<CardItem> StoreItems1 { get; set; }
+        public List<CardItem> StoreItems2 { get; set; }
+        public List<CardItem> BoughtItems { get; set; }
+    }//end of class
+}//end of namespace

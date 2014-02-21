898cc52221181b661e61d2873059dad1b528896f
Joshua Flores <jflores@student.neumont.edu>
2014-02-21 09:02:01 -0700

    8T Applications Lists 1 Listbox ReArrange 4
    Updated initial information and fixed UI errors

--- Projects/WPF_Basic_Interface/Listbox_Drag_and_Drop_Tests/Listbox_Drag_and_Drop_Tests/StoreFunctionalityViewModel.cs
+++ Projects/WPF_Basic_Interface/Listbox_Drag_and_Drop_Tests/Listbox_Drag_and_Drop_Tests/StoreFunctionalityViewModel.cs
@@ -18,6 +18,13 @@ namespace Listbox_Drag_and_Drop_Tests
             var random = new Random(42);
             StoreItems1 = new List<CardItem>
             {
+                new CardItem{ProductName = "George Flores", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Lil Eric Flores", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Big Erick Flores", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Jelly Poole", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Henry Flores", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Stacie Flores", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Dillian Flores", ProductPrice = int.MaxValue},
                 new CardItem{ProductName = "Claymore", ProductPrice = random.Next()},
                 new CardItem{ProductName = "Dao Sabre", ProductPrice = random.Next()},
                 new CardItem{ProductName = "Dirk", ProductPrice = random.Next()},
@@ -26,13 +33,21 @@ namespace Listbox_Drag_and_Drop_Tests
                 new CardItem{ProductName = "Katana", ProductPrice = random.Next()},
             };
             StoreItems2 = new List<CardItem> {
+                new CardItem{ProductName = "Margarita Flores", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Alberto Flores", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Alex Flores", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Susie Diaz", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Noel Diaz", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Noeh Diaz", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Pita Diaz", ProductPrice = int.MaxValue},
+                new CardItem{ProductName = "Bella Flores", ProductPrice = int.MaxValue},
                 new CardItem{ProductName = "Razor", ProductPrice = random.Next()},
                 new CardItem{ProductName = "Rapier", ProductPrice = random.Next()},
                 new CardItem{ProductName = "Seax", ProductPrice = random.Next()},
                 new CardItem{ProductName = "Scimitar", ProductPrice = random.Next()},
                 new CardItem{ProductName = "Xiphos", ProductPrice = random.Next()},
             };
-            BoughtItems = new List<CardItem>();
+            BoughtItems = new List<CardItem>(); //ones who are with me now...
 
         }
     }//end of class

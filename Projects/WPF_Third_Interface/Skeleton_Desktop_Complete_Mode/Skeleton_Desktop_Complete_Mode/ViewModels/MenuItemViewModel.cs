ecf08ac67d2188069cbddc77911fc3819204ba60
Joshua Flores <jflores@student.neumont.edu>
2014-02-21 14:26:45 -0700

    Branched for complete skeleton 2
    Menu created,
    Set up area for zones, and the canvas empty for now...

---
+++ Projects/WPF_Third_Interface/Skeleton_Desktop_Complete_Mode/Skeleton_Desktop_Complete_Mode/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skeleton_Desktop_Complete_Mode.ViewModels
+{
+    public class MenuItemViewModel
+    {
+        //TODO 
+        /*
+         * Abstract out the menu items after you have their functionality down. 
+         */
+    }
+}//end of class

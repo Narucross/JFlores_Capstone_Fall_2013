e0b0543f6fa8752019cfeb71a96db22ee638cb7a
Joshua Flores <jflores@student.neumont.edu>
2014-02-21 11:03:02 -0700

    8T Applications Lists 1 Listbox ReArrange
    What needs left?
    How to specifically drop an mvvm into another

---
+++ Projects/WPF_Basic_Interface/Listbox_Drag_and_Drop_Tests/Listbox_Drag_and_Drop_Tests/Behavior/FrameworkElementDragBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+using Listbox_Drag_and_Drop_Tests.Behavior.Implementation;
+
+namespace Listbox_Drag_and_Drop_Tests.Behavior
+{
+    public class FrameworkElementDragBehavior : Behavior<FrameworkElement>, IMvvmBehavior
+    {
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            IsMvvmParadigm = IsAssociatedObjectMvvm();
+            _mouseDown = false;
+            AssociatedObject.MouseLeftButtonDown -= AssociatedObject_LeftMouseButton_Down_Handler;
+            AssociatedObject.MouseLeftButtonUp -= AssociatedObject_LeftMouseButton_Up_Handler;
+            AssociatedObject.MouseLeave -= AssociatedObject_MouseLeave_Handler;
+        }
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.MouseLeftButtonDown += AssociatedObject_LeftMouseButton_Down_Handler;
+            AssociatedObject.MouseLeftButtonUp += AssociatedObject_LeftMouseButton_Up_Handler;
+            AssociatedObject.MouseMove += AssociatedObject_MouseLeave_Handler;
+        }
+
+        private void AssociatedObject_LeftMouseButton_Down_Handler(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            // On click it needs to retrieve and store the AO.Datacontext
+            if (IsMvvmParadigm)
+            {
+                var dataContext = AssociatedObject.DataContext as IMvvmDraggable;
+
+                _mouseDown = dataContext != null;
+
+            }
+        }
+
+        private bool _mouseDown;
+
+        private void AssociatedObject_MouseLeave_Handler(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (_mouseDown)
+            {
+                DragDrop.DoDragDrop(AssociatedObject, AssociatedObject.DataContext, DragDropEffects.Copy);
+            }
+        }
+
+        private void AssociatedObject_LeftMouseButton_Up_Handler(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _mouseDown = false;
+        }
+
+        #region IMvvMBehavior implementation
+        public bool IsAssociatedObjectMvvm()
+        {
+            return AssociatedObject.DataContext is IMvvmParadigm;
+        }
+        private bool IsMvvmParadigm { get; set; }
+        #endregion
+    }//end of class
+}//end of Namespace
---
+++ Projects/WPF_Basic_Interface/Listbox_Drag_and_Drop_Tests/Listbox_Drag_and_Drop_Tests/Behavior/FrameworkElementDropBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Listbox_Drag_and_Drop_Tests.Behavior.Implementation;
+using System.Windows.Interactivity;
+
+namespace Listbox_Drag_and_Drop_Tests.Behavior
+{
+    public class FrameworkElementDropBehavior : Behavior<FrameworkElement>, IMvvmBehavior
+    {
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.DragEnter -= AssociatedObject_DragEnter_Handler;
+            AssociatedObject.DragOver -= AssociatedObject_DragOver_Handler;
+            AssociatedObject.DragLeave -= AssociatedObject_DragLeave_Handler;
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            IsMvvmParadigm = IsAssociatedObjectMvvm();
+            AssociatedObject.DragEnter += AssociatedObject_DragEnter_Handler;
+            AssociatedObject.DragOver += AssociatedObject_DragOver_Handler;
+            AssociatedObject.DragLeave += AssociatedObject_DragLeave_Handler;
+        }
+
+        private void AssociatedObject_DragEnter_Handler(object sender, DragEventArgs e)
+        {
+            //ToDO what to do when you recieve a drag event
+
+            throw new NotImplementedException();
+        }
+
+        private void AssociatedObject_DragOver_Handler(object sender, DragEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AssociatedObject_DragLeave_Handler(object sender, DragEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool acceptsDrop()
+        {
+            return AssociatedObject.DataContext is IMvvmDroppable;
+        }
+
+        #region IMvvMBehavior implementation
+        public bool IsAssociatedObjectMvvm()
+        {
+            return AssociatedObject.DataContext is IMvvmParadigm;
+        }
+        private bool IsMvvmParadigm { get; set; }
+        #endregion
+    }
+}
